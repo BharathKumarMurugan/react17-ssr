07ca2a5c9839808e579aeef1c0b40780c50a544d
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-09-03 08:53:46 +0530

    Add files via upload

--- webpack.js
+++ webpack.js
@@ -1,80 +1,84 @@
-const path = require("path")
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const nodeExternals = require('webpack-node-externals');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const nodeExternals = require("webpack-node-externals");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
 const server = {
-   name: 'server',
-   entry: './server.js',
-   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'server.js',
-      clean: true,
-      libraryTarget: 'commonjs2'
-   },
-   watch: true,
-   target: 'node', // in order to ignore built-in modules like path, fs, etc.
-   externals: [nodeExternals()],
-   plugins: [
-      new MiniCssExtractPlugin()
-   ],
-   module: {
-      rules: [
-         {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-         },
-         {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, "css-loader"]
-         }
-      ]
-   },
-   optimization: {
-      moduleIds: 'named',
-      chunkIds: 'named',
-   },
-}
-const client = {
-   name: 'client',
-   entry: './client.js',
-   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js',
-      publicPath: ""
-   },
-   target: 'web',
-   module: {
-      rules: [
-         {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-         },
-         {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, "css-loader"]
-         }
-      ]
-   },
-   plugins: [
-      new WebpackManifestPlugin(),
-      new MiniCssExtractPlugin()
-   ],
-   optimization: {
-      moduleIds: 'named',
-      chunkIds: 'named',
-      splitChunks: {
-         cacheGroups: {
-            vendor: {
-               name: "vendors",
-               test: /[\\/]node_modules[\\/]/,
-               chunks: "all",
-            },
-         },
+  name: "server",
+  entry: "./server/server.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "server.js",
+    clean: true,
+    libraryTarget: "commonjs2", // change
+  },
+  watch: true,
+  target: "node",
+  externals: [nodeExternals()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      linkType: "text/css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
       },
-   }
-}
+      {
+        test: /\.css$/i,
+        use: [{loader: MiniCssExtractPlugin.loader}, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    moduleIds: "named",
+    chunkIds: "named",
+  },
+};
+const client = {
+  name: "client",
+  entry: "./client/client.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "",
+  },
+  target: "web",
+  plugins: [
+    new WebpackManifestPlugin(),
+    new MiniCssExtractPlugin({
+      linkType: "text/css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
+      },
+    ],
+  },
 
+  optimization: {
+    moduleIds: "named",
+    chunkIds: "named",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendors",
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
 module.exports = [server, client];
