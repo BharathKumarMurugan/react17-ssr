77bfe5d3988dc847d773604b9e78bf4773aa87b2
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-09-03 08:57:35 +0530

    Add files via upload

--- client/client.js
+++ client/client.js
@@ -1,1 +1,6 @@
+import React from "react";
+import { hydrate } from "react-dom";
+import App from "./components/App";
+import "../common/css/styles.css";
 
+hydrate(<App />, document.getElementById("root"));
